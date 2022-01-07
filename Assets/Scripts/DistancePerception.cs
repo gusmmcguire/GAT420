9eebf0dcf423a8c2661d3f8a66c7694500444086
Gus <gusmcg1856@gmail.com>
2022-01-07 11:53:08 -0700

    day 2

---
+++ Assets/Scripts/DistancePerception.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistancePerception : Perception
+{
+    [SerializeField] float radius = 5;
+    [SerializeField] float maxAngle = 5;
+
+    public override GameObject[] GetGameObjects()
+    {
+        List<GameObject> result = new List<GameObject>();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider collider in colliders)
+        {
+            if (tagName == "" || collider.CompareTag(tagName))
+            {
+                result.Add(collider.gameObject);
+            }
+        }
+
+        return result.ToArray();
+    }
+
+}

576e1581e6704d4e9a05258302a9dcd256384273
Gus <gusmcg1856@gmail.com>
2022-01-10 11:50:46 -0700

    day 3

--- Assets/Scripts/AutonomousAgent.cs
+++ Assets/Scripts/AutonomousAgent.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class AutonomousAgent : Agent
 {
     [SerializeField] Perception perception;
+    [SerializeField] Steering steering;
+    [SerializeField, Tooltip("True if this agent should seek the target instead of run away")] bool shouldSeek;
+
+    public float maxSpeed;
+    public float maxForce;
     
     public Vector3 velocity { get; set; } = Vector3.zero;
 
@@ -17,11 +22,16 @@ public class AutonomousAgent : Agent
         {
             Debug.DrawLine(transform.position, gameObjects[0].transform.position);
 
-            Vector3 force = gameObjects[0].transform.position - transform.position;
+            Vector3 force;
+            if(shouldSeek) force = steering.Seek(this,gameObjects[0]);
+            else force = steering.Flee(this,gameObjects[0]);
             acceleration += force.normalized * 3;
         }
 
         velocity += acceleration * Time.deltaTime;
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
         transform.position += velocity * Time.deltaTime;
+
+        if(velocity.sqrMagnitude > 0.1f) transform.rotation = Quaternion.LookRotation(velocity);
     }
 }

841152eaa488b24a84bd8d0cc1a3718a1b8baa24
Gus <gusmcg1856@gmail.com>
2022-01-31 12:03:31 -0700

    day 11

---
+++ Assets/Scripts/Agent/DistancePerception.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistancePerception : Perception
+{
+    [SerializeField] float radius = 5;
+    [SerializeField] float maxAngle = 5;
+
+    public override GameObject[] GetGameObjects()
+    {
+        List<GameObject> result = new List<GameObject>();
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider collider in colliders)
+        {
+            if (collider.gameObject == gameObject) continue;
+            if (tagName == "" || collider.CompareTag(tagName))
+            {
+                Vector3 direction = (collider.transform.position - transform.position).normalized;
+                float cos = Vector3.Dot(transform.forward, direction);
+                float angle = Mathf.Acos(cos) * Mathf.Rad2Deg;
+
+                if(angle <= maxAngle)
+                {
+                    result.Add(collider.gameObject);
+                }
+
+            }
+        }
+
+        return result.ToArray();
+    }
+
+}
---
+++ Assets/Scripts/StateAgent/StateAgent.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateAgent : Agent
+{
+    [SerializeField] LayerMask layerMask;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, 100, layerMask))
+            {
+                StateAgent[] agents = Agent.GetAgents<StateAgent>();
+                foreach (var agent in agents)
+                {
+                    if (agent.TryGetComponent<Movement>(out Movement movement))
+                    {
+                        movement.MoveTowards(hitInfo.point);
+                    }
+                }
+            }
+
+        }
+    }
+}
+
